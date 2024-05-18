a3e0f08a9d3779965fa0f1760751878370c0afe1
PathumLakshan <92371888+pathuGIT@users.noreply.github.com>
2024-05-19 01:01:14 +0530

    updating header

---
+++ content/header.php
@@ -0,0 +1,26 @@
+
+<header>
+    <div class="topnav" >
+        <li id="logo"><img src="img/logo.png"></li>
+        <!-- <li id="brand-name">Train<span style="color: rgb(0, 0, 0);">Go</span></li> -->
+        <li class="social">
+            <a href=""><i class="fab fa-facebook-f"></i></a>
+            <a href=""><i class="fab fa-instagram"></i></a>
+        </li>
+    </div>
+    <nav class="bottomnav " id="myTopnav">
+        <ul>
+            <li>
+                <a id="icon">
+                    <i class="fa fa-bars"></i>
+                </a>
+            </li>
+            <li class="f1">
+                <a href="index.html">Home</a>
+            </li>
+            <li><a href="login.html">Packages</a></li>
+            <li><a href="contact_us.html">Custom Your Package</a></li>
+            <li><a href="aboutus.html">Contact</a></li>
+        </ul>
+    </nav>
+</header>
--- index.php
+++ index.php
@@ -1,36 +1,109 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Tour With RK</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="css/index.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<body>
-<header>
-    <div class="topnav" >
-        <li id="logo"><img src="img/logo.png"></li>
-        <!-- <li id="brand-name">Train<span style="color: rgb(0, 0, 0);">Go</span></li> -->
-        <li class="social">
-            <a href=""><i class="fab fa-facebook-f"></i></a>
-            <a href=""><i class="fab fa-instagram"></i></a>
-        </li>
+    <head>
+        <meta charset="UTF-8">
+        <title>Tour With RK</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="css/index.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+
+    <?php require_once "content/header.php"; ?>
+
+    <div class="content">
+        <?php
+    //     echo "<script>
+    //     document.addEventListener('DOMContentLoaded', function() {
+    //         myFunction();
+    //     });
+    // </script>";
+
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+            echo"Hello<br>";
+        ?>
     </div>
-    <nav class="bottomnav" id="myTopnav">
-        <ul>
-            <li class="f1">
-                <a href="index.html">Home</a>
-                <a href="javascript:void(0);" id="icon" onclick="myFunction()">
-                    <i class="fa fa-bars"></i>
-                </a>
-            </li>
-            <li><a href="login.html">Packages</a></li>
-            <li><a href="contact_us.html">Custom Your Package</a></li>
-            <li><a href="aboutus.html">Contact</a></li>
-        </ul>
-    </nav>
-</header>
-</body>
+
+
+    <?php require_once "content/footer.php"; ?>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            window.onscroll = function() {myFunction2()};
+            var navbar = document.getElementById("myTopnav");
+            var sticky = navbar.offsetTop;
+
+            function myFunction2() {
+                if (window.pageYOffset >= sticky) {
+                    navbar.classList.add("sticky");
+                } else {
+                    navbar.classList.remove("sticky");
+                }
+            }
+        });
+    </script>
+    </body>
 </html>
